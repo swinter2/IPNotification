d8e59951011b4c0638a6068d48d0ad15d25a262d
Sam Winter <sam.winter@gmail.com>
2015-04-27 20:09:15 -0400

    First commit!

---
+++ IPNotification2/CheckIPService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Configuration;
+
+namespace IPNotification2
+{
+    public partial class CheckIPService
+    {
+        private const string _checkURI = "CheckURI";
+        private const string _emailFrom = "EmailFrom";
+        private const string _emailTo = "EmailTo";
+        private const string _emailSubject = "EmailSubject";
+
+        private Regex _ipPattern = new Regex(@"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", RegexOptions.Multiline);
+
+        protected Uri CheckUri { get; set; }
+
+        protected MailAddress EmailFrom { get; set; }
+        protected MailAddress EmailTo { get; set; }
+        protected string EmailSubject { get; set; }
+
+        public CheckIPService()
+        {
+            CheckUri = new Uri(ConfigurationManager.AppSettings[_checkURI]);
+            EmailFrom = new MailAddress(ConfigurationManager.AppSettings[_emailFrom]);
+            EmailTo = new MailAddress(ConfigurationManager.AppSettings[_emailTo]);
+            EmailSubject = ConfigurationManager.AppSettings[_emailSubject];
+        }
+
+        public void Check()
+        {
+            try
+            {
+                var req = WebRequest.Create(CheckUri);
+                var res = req.GetResponse();
+                var stream = res.GetResponseStream();
+                var sr = new StreamReader(stream);
+                var body = sr.ReadToEnd();
+                sr.Close();
+                stream.Close();
+
+                var matches = _ipPattern.Matches(body);
+
+                // Send these IP address matches in an email to me.
+                var msgBody = new StringBuilder();
+                msgBody.AppendLine("IP Addresses:");
+
+                foreach (var match in matches)
+                {
+                    msgBody.AppendLine(match.ToString());
+                }
+
+                var msg = new MailMessage(EmailFrom.ToString(), EmailTo.ToString(), EmailSubject, msgBody.ToString());
+                var smtp = new SmtpClient();
+                smtp.Send(msg);
+            }
+            catch (Exception ex)
+            {
+                EventLog.WriteEntry(Assembly.GetExecutingAssembly().FullName, string.Format("{0}\n\n{1}", ex.Message, ex.StackTrace), EventLogEntryType.Error);
+            }
+        }
+
+    }
+}
---
+++ IPNotification2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPNotification2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new CheckIPService();
+            service.Check();
+        }
+    }
+}
